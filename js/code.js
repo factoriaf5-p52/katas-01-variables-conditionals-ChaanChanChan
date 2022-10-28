be9af24736594471f56c5c7661bde55e9be60dfb
Chander <alexandertakepics@gmail.com>
2022-10-28 14:07:29 +0200

    done

--- js/code.js
+++ js/code.js
@@ -1,7 +1,17 @@
 function evenOrOdd(n) {
-    return "";
+    if (n % 2 == 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
 }
 
 function greeting(name) {
-    return "";
+    if (name === "Javi") {
+        return 'Hola, ' + name + '!';
+    } else if (name === "gente") {
+        return 'Hola, ' + name + '!';
+    } else {
+        return 'Hola, ' + name + '!';
+    }
 }
--- js/tests.js
+++ js/tests.js
@@ -2,35 +2,37 @@ const chai = window.chai
 const expect = chai.expect
 
 describe('Las variables en Javascript', () => {
-  it('pueden contener strings', () => {
-    // Añade la línea que falta
-    const a = "Javascript"
-    expect(a).to.equal("Javascript");
-  })
-  it('pueden contener números', () => {
-    // Añade la línea que falta
-
-    expect(b).to.equal(14.64);
-  })
-  it('pueden contener booleanos', () => {
-    // Añade la línea que falta
-
-    expect(c).to.equal(false);
-  })
+    it('pueden contener strings', () => {
+        // Añade la línea que falta
+        const a = "Javascript"
+        expect(a).to.equal("Javascript");
+    })
+    it('pueden contener números', () => {
+        // Añade la línea que falta
+        const b = 14.64
+        expect(b).to.equal(14.64);
+    })
+    it('pueden contener booleanos', () => {
+        // Añade la línea que falta
+        const c = false
+        expect(c).to.equal(false);
+    })
 })
 
 describe('Comparaciones en Javascript', () => {
     it("usando ===", () => {
         const number = 20;
         //completa la asignación usando number y el comparador ===
-
+        const number2 = 5;
+        const expresion = number === number2;
         expect(expresion).to.equal(false)
     })
 
     it("usando !==", () => {
         const language = "Javascript";
         //completa la asignación usando language y el comparador !==
-
+        const language2 = "Javascript";
+        const expresion = language !== language2;
         expect(expresion).to.equal(false)
     })
 })
@@ -38,18 +40,18 @@ describe('Comparaciones en Javascript', () => {
 describe("Operaciones aritméticas", () => {
     it("Usando suma", () => {
         const a = 10;
-
-        expect(a+b).to.equal(100);
+        const b = 90;
+        expect(a + b).to.equal(100);
     })
     it("Usando multiplicación", () => {
         const a = 10;
-
-        expect(a*b).to.equal(13);
+        const b = 1.3;
+        expect(a * b).to.equal(13);
     })
     it("Usando división", () => {
         const a = 10;
-
-        expect(a/b).to.equal(2.5);
+        const b = 4;
+        expect(a / b).to.equal(2.5);
     })
 })
 
@@ -57,14 +59,14 @@ describe('Algebra booleana', () => {
     it("Usando el operador AND (&&)", () => {
         const a = true;
         //completa la asignación de b
-
+        const b = true
         const expression = a && b;
         expect(expression).to.equal(true);
     })
     it("Usando el operador OR (||)", () => {
         const a = false;
         //completa la asignación de b
-
+        const b = false
         const expression = a || b;
         expect(expression).to.equal(false);
     })
@@ -79,7 +81,7 @@ describe("Ejemplos de funciones", () => {
         expect(evenOrOdd(13)).to.equal("odd")
     })
 
-    it ("Función que nos devuelve un saludo para cada nombre", () => {
+    it("Función que nos devuelve un saludo para cada nombre", () => {
         expect(greeting("Javi")).to.equal("Hola, Javi!")
         expect(greeting("gente")).to.equal("Hola, gente!")
         expect(greeting("Mary")).to.equal("Hola, Mary!")
